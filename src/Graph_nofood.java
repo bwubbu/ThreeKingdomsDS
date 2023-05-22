95e8d1851943e23ca8fadfd16fc29b8ea7a9306b
Ditto226 <116065154+Ditto226@users.noreply.github.com>
2023-05-22 13:17:24 +0800

    Update Graph_nofood.java

--- src/Graph_nofood.java
+++ src/Graph_nofood.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Graph2 {
+public class Graph_nofood {
     private int numNodes;
     private List<Integer>[] adjacencyList;
 
