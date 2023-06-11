0c25509c33f1bdb388d823b8eb9d2be872f24376
Blub <101506741+bwubbu@users.noreply.github.com>
2023-06-11 15:36:01 +0800

    Add files via upload

---
+++ src/BattleshipClustersBFS.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+//using bfs
+public class BattleshipClusters {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 0, 0},
+            {1, 0, 0, 0},
+            {1, 0, 1, 1},
+            {1, 0, 0, 0}
+        };
+
+        int clusters = findBattleshipClusters(matrix);
+        System.out.println(clusters + " cluster(s)");
+    }
+
+    private static int findBattleshipClusters(int[][] matrix) {
+        int clusters = 0;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 1 && !visited[i][j]) {
+                    bfs(matrix, visited, i, j);
+                    clusters++;
+                }
+            }
+        }
+
+        return clusters;
+    }
+
+    private static void bfs(int[][] matrix, boolean[][] visited, int startRow, int startCol) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int[] rowOffsets = { -1, 1, 0, 0 }; // Up, Down, Left, Right
+        int[] colOffsets = { 0, 0, -1, 1 }; // Up, Down, Left, Right
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { startRow, startCol });
+        visited[startRow][startCol] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int newRow = row + rowOffsets[i];
+                int newCol = col + colOffsets[i];
+
+                if (isValidPosition(newRow, newCol, rows, cols) && !visited[newRow][newCol] && matrix[newRow][newCol] == 1) {
+                    queue.offer(new int[] { newRow, newCol });
+                    visited[newRow][newCol] = true;
+                }
+            }
+        }
+    }
+
+    private static boolean isValidPosition(int row, int col, int rows, int cols) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
+    }
+}
---
+++ src/MazeEscapeAstar.java
@@ -0,0 +1,164 @@
+import java.util.*;
+
+public class MazeEscape {
+    public static void main(String[] args) {
+        int[][] maze = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+            {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1},
+            {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+            {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1},
+            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        findPath(maze);
+    }
+
+    private static void findPath(int[][] maze) {
+        int startRow = -1;
+        int startCol = -1;
+
+        // Find the starting position
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (maze[i][j] == 2) {
+                    startRow = i;
+                    startCol = j;
+                    break;
+                }
+            }
+        }
+
+        // Perform A* search to find the path
+        if (startRow != -1 && startCol != -1) {
+            List<Node> path = aStarSearch(maze, startRow, startCol);
+            if (path != null) {
+                System.out.println("Path found!");
+                markPathInMaze(maze, path);
+                printMaze(maze);
+            } else {
+                System.out.println("No path found.");
+            }
+        } else {
+            System.out.println("Starting position not found.");
+        }
+    }
+
+    private static List<Node> aStarSearch(int[][] maze, int startRow, int startCol) {
+        int numRows = maze.length;
+        int numCols = maze[0].length;
+        int[][] distance = new int[numRows][numCols];
+        boolean[][] visited = new boolean[numRows][numCols];
+        Node[][] parent = new Node[numRows][numCols];
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(n -> n.f));
+
+        for (int i = 0; i < numRows; i++) {
+            Arrays.fill(distance[i], Integer.MAX_VALUE);
+        }
+
+        distance[startRow][startCol] = 0;
+        queue.offer(new Node(startRow, startCol, 0, 0));
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int row = current.row;
+            int col = current.col;
+
+            if (maze[row][col] == 3) {
+                // Exit found, reconstruct the path
+                List<Node> path = new ArrayList<>();
+                while (parent[row][col] != null) {
+                    path.add(0, parent[row][col]);
+                    Node prev = parent[row][col];
+                    row = prev.row;
+                    col = prev.col;
+                }
+                return path;
+            }
+
+            visited[row][col] = true;
+
+            // Explore neighboring positions
+            exploreNeighbor(maze, distance, visited, parent, queue, row - 1, col, current, 1); // Up
+            exploreNeighbor(maze, distance, visited, parent, queue, row + 1, col, current, 1); // Down
+            exploreNeighbor(maze, distance, visited, parent, queue, row, col - 1, current, 1); // Left
+            exploreNeighbor(maze, distance, visited, parent, queue, row, col + 1, current, 1); // Right
+        }
+
+        return null; // No path found
+    }
+
+    private static void exploreNeighbor(int[][] maze, int[][] distance, boolean[][] visited, Node[][] parent,
+            PriorityQueue<Node> queue, int row, int col, Node current, int cost) {
+        int numRows = maze.length;
+        int numCols = maze[0].length;
+
+        if (row >= 0 && row < numRows && col >= 0 && col < numCols && maze[row][col] != 1 && !visited[row][col]) {
+            int newDistance = current.g + cost;
+
+            if (newDistance < distance[row][col]) {
+                distance[row][col] = newDistance;
+                parent[row][col] = current;
+                int h = calculateHeuristic(row, col, maze);
+                queue.offer(new Node(row, col, newDistance, newDistance + h));
+            }
+        }
+    }
+
+    private static int calculateHeuristic(int row, int col, int[][] maze) {
+        int exitRow = -1;
+        int exitCol = -1;
+
+        // Find the exit position
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[0].length; j++) {
+                if (maze[i][j] == 3) {
+                    exitRow = i;
+                    exitCol = j;
+                    break;
+                }
+            }
+        }
+
+        // Calculate the Manhattan distance
+        return Math.abs(row - exitRow) + Math.abs(col - exitCol);
+    }
+
+    private static void markPathInMaze(int[][] maze, List<Node> path) {
+        for (Node node : path) {
+            int row = node.row;
+            int col = node.col;
+            if (maze[row][col] != 2 && maze[row][col] != 3) {
+                maze[row][col] = 9; // Mark the path with 9
+            }
+        }
+    }
+
+    private static void printMaze(int[][] maze) {
+        for (int[] row : maze) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static class Node {
+        int row;
+        int col;
+        int g; // Cost from the start node
+        int f; // g + heuristic
+
+        Node(int row, int col, int g, int f) {
+            this.row = row;
+            this.col = col;
+            this.g = g;
+            this.f = f;
+        }
+    }
+}
